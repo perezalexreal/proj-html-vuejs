9b8d3316045ffdb254f880f9a5de37b921d8f7b2
perezalexreal <perezalexreal@gmail.com>
2022-02-08 11:20:56 +0100

    Sviluppo header

---
+++ proj-html-vuejs/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+<header-box :nav="nav" :logoPath="logoPath"/>
+  </div>
+</template>
+
+<script>
+import HeaderBox from './components/HeaderBox.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBox
+  },
+  data () {
+    return {
+      logoPath: '../assets/logo-sidearea-1-1.png',
+      nav : [
+        'Home',
+        'Pages',
+        'Portfolio',
+        'Blog',
+        'Shop',
+        'Elements',
+      ],
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ proj-html-vuejs/src/components/HeaderBox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="header-box">
+            <img class="logo-img" :src="logoPath" alt="">
+            <div class="container-links">
+                <ul v-for="(link,index) in nav" :key="index">
+                    <li> {{ link }} </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+    props: {
+        nav: Array,
+        logoPath: String
+    }
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+.logo-img {
+    width: 300px;
+}
+</style>
